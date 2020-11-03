a7e69e5b3eb1fd2d79475382fd7b34564c5b8e82
Artur Tsitou <itsitou88@gmail.com>
2020-11-03 10:52:08 +0300

    before adaptife

--- Max-MITRAVEL/js/main.js
+++ Max-MITRAVEL/js/main.js
@@ -1,6 +1,63 @@
-let swiper = new Swiper('.slider-container', {
+// Переменные
+
+const swiper1 = document.querySelector('.slider-container'),
+	swiper2 = document.querySelector('.swiper-container'),
+	burger = document.querySelector('.burger'),
+	close = document.querySelector('.menu__close'),
+	menu = document.querySelector('.menu'),
+	playButtonsFirst = document.querySelectorAll('.main-slider__play');
+
+//  Второй слайдер
+let swiperSlider1 = new Swiper(swiper1, {
 	centeredSlides: true,
 	slidesPerView: 'auto',
 	loop: true,
 	spaceBetween: 105,
+});
+// Первый слайдер
+let swiperSlider2 = new Swiper(swiper2, {
+	centeredSlides: true,
+	slidesPerView: '1',
+	loop: true,
+	spaceBetween: 10,
+	fadeEffect: {
+		crossFade: true
+	},
+	effect: 'fade',
+	navigation: {
+		nextEl: '.btn-right',
+		prevEl: '.btn-left',
+	},
+});
+// Скрипт для работы видео в слайдере
+swiperSlider2.on('transitionEnd', function () {
+	let videos = document.querySelectorAll('.first__slider video');
+	videos.forEach((el) => {
+		el.pause();
+		el.currentTime = 0;
+	});
+	playButtonsFirst.forEach((el) => {
+		el.style.display = 'block';
+	});
+});
+// Бургер меню
+burger.addEventListener('click', () => {
+	menu.classList.add('menu--visible');
+});
+
+close.addEventListener('click', () => {
+	menu.classList.remove('menu--visible');
+});
+
+
+
+playButtonsFirst.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+		video.play();
+		e.currentTarget.style.display = 'none';
+		setTimeout(() => {
+			video.volume = 0.5;
+		}, 1000);
+	});
 });
