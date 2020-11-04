822765265baab465bbc5473426a9577eccb337be
Artur Tsitou <itsitou88@gmail.com>
2020-11-04 06:40:32 +0300

    add js libs

--- Max-MITRAVEL/js/main.js
+++ Max-MITRAVEL/js/main.js
@@ -61,3 +61,59 @@ playButtonsFirst.forEach((el) => {
 		}, 1000);
 	});
 });
+
+// inputMask
+let selector = document.querySelectorAll('input[type=tel]');
+
+let im = new Inputmask('+7 (999) 999-99-99');
+
+im.mask(selector);
+
+
+// validate forms
+let validateForms = function (selector, rules) {
+
+	new window.JustValidate(selector, {
+		rules: rules,
+		submitHandler: function (form, values, ajax) {
+			var formData = new FormData(form);
+
+			var xhr = new XMLHttpRequest();
+
+
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === 4) {
+
+					if (xhr.status === 200) {
+						console.log('Отправлено!')
+					} else {
+
+					}
+				}
+			}
+
+			// Add any event handlers here...
+			xhr.open('POST', "mail.php", true);
+			xhr.send(formData);
+
+			form.reset();
+		},
+	});
+}
+
+// validateForms('.mailing__form', { email: { required: true, email: true } }, 'mailing-window', 'materialy s proshloj konferencii');
+validateForms('.newsletter__form', {
+	email: {
+		required: true,
+		email: true
+	},
+	tel: {
+		required: true
+	}
+});
+validateForms('.subs-form', {
+	email: {
+		required: true,
+		email: true
+	}
+});
